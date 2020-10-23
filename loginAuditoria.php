5d29057be1f8c53558e0d41291814e7cc2b97136
Abigail Martínez <73217246+Mina72@users.noreply.github.com>
2020-10-22 18:52:50 -0600

    Create loginAuditoria.php

---
+++ loginAuditoria.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+    <!-- <link rel="stylesheet" href="css/estilo.css"> -->
+
+    <title>AUDITORIA</title>
+  </head>
+  <body><br><center>
+    <h3>INGRERSO</h3>
+    <form class="" action="flogin.php" method="post">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="col-md-4"><br>
+              <div class="col-md-12">
+                <label for="">Usuario:</label>
+                <input class="form-control" type="text" name="usu" placeholder="Nombre">
+              </div>
+              <div class="col-md-12">
+                <label for="">Password:</label>
+                <input class="form-control" type="password" name="pass" placeholder="Password">
+              </div>
+              <div class="col-md-12"><hr>
+                <input class="form-control btn btn-primary btn-block" type="submit" value="Ingresar">
+              </div><br><br>
+
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    </form>
+  </body>
+</html>
